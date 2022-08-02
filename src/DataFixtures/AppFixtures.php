6337316a6a4599e4525e5e855965ee78ad548638
SOUISSI Mokhles <mokhles.souissi@soprahr.com>
2022-08-02 13:59:06 +0100

    135802082022

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -19,6 +19,11 @@ class AppFixtures extends Fixture
         $marque->setImgurl("http://assets.stickpng.com/images/580b57fcd9996e24bc43c46e.png");
         $marque->setDescription("BMW (ou Bayerische Motoren Werke en allemand, litt. « Manufacture bavaroise de moteurs »), est un constructeur allemand d'automobiles haut-de-gamme, sportives et luxueuses et de motos, après avoir été un grand constructeur de moteurs d'avions. L'entreprise a été fondée en 1916 par Gustav Otto et Karl Friedrich Rapp");
         $manager->persist($marque);
+        $model = new Models();
+        $model->setNom("I8");
+        $model->setAnnee("2014-2020");
+        $model->setCouleur("Bleu");
+        $manager->persist($model);
         $manager->flush();
     }
 }
